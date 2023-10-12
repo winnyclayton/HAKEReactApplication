1ba2e9fa0589f1989b90f4673a536e3b48ef62d0
winny <9036@ait.nsw.edu.au>
2023-10-13 10:13:20 +1100

    config double checked

--- config/Config.js
+++ config/Config.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAOJ-WMPfuOcVJTxDh5S2FZpccrb74PdtE",
-    authDomain: "expo23-1f526.firebaseapp.com",
-    projectId: "expo23-1f526",
-    storageBucket: "expo23-1f526.appspot.com",
-    messagingSenderId: "706031052816",
-    appId: "1:706031052816:web:45eb11a0009b77b2fc4295"
-  };
+  apiKey: "AIzaSyAOJ-WMPfuOcVJTxDh5S2FZpccrb74PdtE",
+  authDomain: "expo23-1f526.firebaseapp.com",
+  projectId: "expo23-1f526",
+  storageBucket: "expo23-1f526.appspot.com",
+  messagingSenderId: "706031052816",
+  appId: "1:706031052816:web:45eb11a0009b77b2fc4295"
+};
