3c7a65adfe04235004d3242dffc10bd86d336d5b
winny <9036@ait.nsw.edu.au>
2023-10-13 10:13:08 +1100

    copied johannes code

--- App.js
+++ App.js
@@ -1,16 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-//firebase
-import { firebaseConfig } from './config/Config';
-import { initializeApp } from 'firebase/app';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { useState } from 'react'
+// contexts
+import { AuthContext } from './contexts/AuthContext'
+// firebase
+import { firebaseConfig } from './config/Config'
+import { initializeApp } from 'firebase/app'
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  onAuthStateChanged 
+} from "firebase/auth"
+// react navigation
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+// screens
+import { Signup } from './screens/Signup'
+import { Signin } from './screens/Signin'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   const FBapp = initializeApp( firebaseConfig )
+  const FBauth = getAuth( FBapp)
+  // state
+  const[auth,setAuth] = useState()
+
+  // authentication observer
+  onAuthStateChanged( FBauth, ( user ) => {
+    if( user ) {
+      // user is authenticated
+      setAuth( user )
+    }
+    else {
+      // user is not authenticated
+      setAuth( null )
+    }
+  })
+
+  const Register = ( email, password ) => {
+    return new Promise( ( resolve, reject ) => {
+      createUserWithEmailAndPassword( FBauth, email, password)
+      .then( (response) => resolve(response) )
+      .catch( (err) => reject(err) )
+    })
+  }
+
+  const Login = () => {}
+
   return (
-    <View style={styles.container}>
-      <Text>Hello Winny!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <AuthContext.Provider value={auth}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Sign up">
+            { (props) => <Signup handler={Register} /> }
+          </Stack.Screen>
+          <Stack.Screen name="Sign in">
+            { (props) => <Signin handler={Login} /> }
+          </Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </AuthContext.Provider>
   );
 }
 
