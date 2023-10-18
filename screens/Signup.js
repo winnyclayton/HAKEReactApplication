bec3468788a5c10ee99e3e83bb6cf4761817d0e2
winny <9036@ait.nsw.edu.au>
2023-10-19 09:58:43 +1100

    style changes

--- screens/Signup.js
+++ screens/Signup.js
@@ -4,13 +4,37 @@ import { AuthContext } from '../contexts/AuthContext'
 import { useNavigation } from '@react-navigation/native'
 
 export function Signup( props ) {
-  const[email,setEmail] = useState()
-  const[password,setPassword] = useState()
+  const[email,setEmail] = useState('')
+  const[password,setPassword] = useState('')
+
+  const[ validEmail, setValidEmail ] = useState(false)
+  const[ validPassword, setValidPassword ] = useState(false)
 
   const navigation = useNavigation()
   const auth = useContext(AuthContext)
 
-  // useEffect(() => { console.log(email) }, [email])
+  //check the value of email
+  useEffect( () => {
+    if( email.indexOf('@') > 0 ) {
+    setValidEmail( true )
+    }
+    else {
+      setValidEmail( false )
+      //code an alert here for the user 'please enter a valid email'
+    }
+    }, [email])
+    
+    //check the value of password
+    useEffect( () => {
+    if ( password.length >= 8 ) {
+      setValidPassword(true)
+    }
+    else {
+      setValidPassword(false)
+      //code an alert here for the user 'please enter 8 digits or more'
+    }
+    }, [password])
+
   const submitHandler = () => {
     props.handler( email, password )
     .then( ( user ) => {
@@ -24,7 +48,7 @@ export function Signup( props ) {
   return(
     <View style={ styles.container }>
       <View style={ styles.form }>
-        <Text style={ styles.title }>Register for an account</Text>
+        <Text style={ styles.title }>Sign up for an account</Text>
         <Text>Email</Text>
         <TextInput 
           style={styles.input} 
@@ -40,11 +64,21 @@ export function Signup( props ) {
           value={password}
           onChangeText={(val) => setPassword(val)}
         />
-        <Pressable style={ styles.button } onPress={() => submitHandler() }>
-          <Text style={ styles.button.text }>Sign up</Text>
+        <Pressable
+        //if the email/password are valid then use styles.button, otherwise use styles.disabledbutton
+        style={ (validEmail && validPassword) ? styles.button : styles.disabledButton }
+        onPress={() => submitHandler() }
+        //if valid email/password is true, then set disabled to false otherwise set disabled to true
+        disabled={(validEmail && validPassword) ? false : true }
+        >
+        <Text style={ styles.button.text }>Sign up</Text>
+        
         </Pressable>
-        <Pressable onPress={() => navigation.navigate("Sign in")}>
-          <Text>Go to sign in</Text>
+        <Pressable
+        style={styles.authLink}
+        onPress={() => navigation.navigate("Sign in")}
+        >
+        <Text style={styles.authLink.text}>Already have an account? Login</Text>
         </Pressable>
       </View>
     </View>
@@ -80,6 +114,22 @@ const styles = StyleSheet.create({
     text: {
       color: '#eeeeee',
       textAlign: 'center',
+    }
+  },
+
+  disabledButton: {
+    backgroundColor: '#666666',
+    padding: 5,
+    text: {
+      color: '#eeeeee',
+      textAlign: 'center',
+    }
+  },
+
+  authLink: {
+    marginTop: 10,
+    text: {
+      textAlign: 'center'
     }
   }
 })
