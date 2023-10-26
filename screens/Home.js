6180e1522f7ac25f166ebc91e8d8e16039a9f236
winny <9036@ait.nsw.edu.au>
2023-10-26 13:15:03 +1100

    home

---
+++ screens/Home.js
@@ -0,0 +1,47 @@
+import { View, Text, StyleSheet } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { useNavigation } from '@react-navigation/native'
+import IonIcons from '@expo/vector-icons/Ionicons'
+
+import { AuthContext } from '../contexts/AuthContext'
+import { useContext, useState, useEffect } from 'react'
+
+import { Data } from './Data'
+import { Profile } from './Profile'
+
+const Tab = createBottomTabNavigator()
+
+export function Home( props ) {
+  const [ email, setEmail ] = useState()
+
+  const Auth = useContext(AuthContext)
+  const navigation = useNavigation()
+
+  useEffect( () => {
+    if( Auth.currentUser ) {
+      setEmail( Auth.currentUser.email )
+    }
+    else {
+      navigation.reset( { index: 0, routes: [ {name: "Sign in"} ] })
+    }
+  })
+
+  const DataOptions = {
+    tabBarLabel: "Home",
+    tabBarIcon: ({ color }) => <IonIcons name="home" color={color} size={20} />,
+    title: "Home"
+  }
+
+  const ProfileOptions = {
+    tabBarLabel: "Profile",
+    tabBarIcon: ({ color }) => <IonIcons name="person" color={color} size={20} />,
+    title: email
+  }
+
+  return (
+    <Tab.Navigator initialRouteName="Data">
+      <Tab.Screen name="Data" component={Data} options={ DataOptions } />
+      <Tab.Screen name="Profile" component={Profile} options={ProfileOptions} />
+    </Tab.Navigator>
+  )
+}
