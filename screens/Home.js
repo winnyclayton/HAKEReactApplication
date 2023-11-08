81467d502b45190b7b0b0336daf8c1f50f5bca2a
winny <9036@ait.nsw.edu.au>
2023-11-08 14:21:23 +1100

    nothing

--- screens/Home.js
+++ screens/Home.js
@@ -9,6 +9,7 @@ import { useContext, useState, useEffect } from 'react'
 import { Data } from './Data'
 import { Profile } from './Profile'
 import { Upload } from './Upload'
+import { Browse } from './Browse'
 
 const Tab = createBottomTabNavigator()
 
@@ -30,26 +31,59 @@ export function Home( props ) {
   const UploadOptions = {
     tabBarLabel: "Upload",
     tabBarIcon: ({ color }) => <IonIcons name="add-outline" color={color} size={20} />,
-    title: "Upload Portal"
+    title: "Upload Portal",
+    headerStyle: {
+      backgroundColor: '#E5EDD5', //header background colour
+    },
     }
 
   const DataOptions = {
     tabBarLabel: "Home",
     tabBarIcon: ({ color }) => <IonIcons name="home" color={color} size={20} />,
-    title: "Home"
+    title: "Home",
+    headerStyle: {
+      backgroundColor: '#E5EDD5', //header background colour
+    },
+  }
+
+  const BrowseOptions = {
+    tabBarLabel: "Browse",
+    tabBarIcon: ({ color }) => <IonIcons name="eye-outline" color={color} size={20} />,
+    title: "Browse",
+    headerStyle: {
+      backgroundColor: '#E5EDD5', //header background colour
+    },
   }
 
   const ProfileOptions = {
     tabBarLabel: "Profile",
     tabBarIcon: ({ color }) => <IonIcons name="person" color={color} size={20} />,
-    title: email
+    title: email,
+    headerStyle: {
+      backgroundColor: '#E5EDD5', //header background colour
+    },
   }
 
   return (
-    <Tab.Navigator initialRouteName="Data">
+    <Tab.Navigator
+    initialRouteName="Data"
+    screenOptions={{
+      tabBarActiveTintColor: '#DAF6B2', //change active tab colour to green
+      tabBarInactiveTintColor: 'white', // change inactive tab colour to white
+      tabBarStyle: { backgroundColor: '#396C4D' }, //change background colour nav bar
+    }}
+    >
       <Tab.Screen name="Upload" component={Upload} options={UploadOptions} />
       <Tab.Screen name="Data" component={Data} options={ DataOptions } />
+      <Tab.Screen name="Browse" component={Browse} options={BrowseOptions} />
       <Tab.Screen name="Profile" component={Profile} options={ProfileOptions} />
+
     </Tab.Navigator>
   )
 }
+
+
+
+
+ 
+
